b0cfa0e36499981f872296c02ba519842636cd2c
Paul Lam <paul@quantisan.com>
2013-01-27 10:44:16 -0500

    enable portfolio.R with long-only

--- script/portfolio.R
+++ script/portfolio.R
@@ -32,11 +32,12 @@ portfolio.returns <- function(index.symbol, names, ...) {
 # 
 # Functions for portfolio analysis
 # to be used in Introduction to Computational Finance & Financial Econometrics
-# last updated: November 7, 2000 by Eric Zivot
+# last updated: August 8, 2012 by Hezky Varon
+#               November 7, 2000 by Eric Zivot
 #               Oct 15, 2003 by Tim Hesterberg
 #               November 18, 2003 by Eric Zivot
 #               November 9, 2004 by Eric Zivot
-#		            November 9, 2008 by Eric Zivot
+#  	            November 9, 2008 by Eric Zivot
 #               August 11, 2011 by Eric Zivot
 #
 # Functions:
@@ -46,54 +47,57 @@ portfolio.returns <- function(index.symbol, names, ...) {
 #	3. tangency.portfolio			  compute tangency portfolio
 #	4. efficient.frontier			  compute Markowitz bullet
 #	5. getPortfolio					    create portfolio object
-#
 
-getPortfolio <- function(er, cov.mat, weights) {
-  # contruct portfolio object
-  #
-  # inputs:
-  # er				   N x 1 vector of expected returns
-  # cov.mat  		 N x N covariance matrix of returns
-  # weights			 N x 1 vector of portfolio weights
-  #
-  # output is portfolio object with the following elements
-  # call				original function call
-  # er				  portfolio expected return
-  # sd				  portfolio standard deviation
-  # weights			N x 1 vector of portfolio weights
-  #
-  call <- match.call()
-  
-  #
-  # check for valid inputs
-  #
-  asset.names <- names(er)
-  weights <- as.vector(weights)
-  names(weights) = names(er)
-  er <- as.vector(er)					# assign names if none exist
-  if(length(er) != length(weights))
-    stop("dimensions of er and weights do not match")
-  cov.mat <- as.matrix(cov.mat)
-  if(length(er) != nrow(cov.mat))
-    stop("dimensions of er and cov.mat do not match")
-  if(any(diag(chol(cov.mat)) <= 0))
-    stop("Covariance matrix not positive definite")
-  
-  #
-  # create portfolio
-  #
-  er.port <- crossprod(er,weights)
-  sd.port <- sqrt(weights %*% cov.mat %*% weights)
-  ans <- list("call" = call,
-              "er" = as.vector(er.port),
-              "sd" = as.vector(sd.port),
-              "weights" = weights) 
-  class(ans) <- "portfolio"
-  ans
-}
+stopifnot("package:quadprog" %in% search()  ||  require("quadprog",quietly = TRUE) )
+
+getPortfolio <-
+  function(er, cov.mat, weights)
+  {
+    # contruct portfolio object
+    #
+    # inputs:
+    # er				   N x 1 vector of expected returns
+    # cov.mat  		 N x N covariance matrix of returns
+    # weights			 N x 1 vector of portfolio weights
+    #
+    # output is portfolio object with the following elements
+    # call				original function call
+    # er				  portfolio expected return
+    # sd				  portfolio standard deviation
+    # weights			N x 1 vector of portfolio weights
+    #
+    call <- match.call()
+    
+    #
+    # check for valid inputs
+    #
+    asset.names <- names(er)
+    weights <- as.vector(weights)
+    names(weights) = names(er)
+    er <- as.vector(er)					# assign names if none exist
+    if(length(er) != length(weights))
+      stop("dimensions of er and weights do not match")
+    cov.mat <- as.matrix(cov.mat)
+    if(length(er) != nrow(cov.mat))
+      stop("dimensions of er and cov.mat do not match")
+    if(any(diag(chol(cov.mat)) <= 0))
+      stop("Covariance matrix not positive definite")
+    
+    #
+    # create portfolio
+    #
+    er.port <- crossprod(er,weights)
+    sd.port <- sqrt(weights %*% cov.mat %*% weights)
+    ans <- list("call" = call,
+                "er" = as.vector(er.port),
+                "sd" = as.vector(sd.port),
+                "weights" = weights) 
+    class(ans) <- "portfolio"
+    ans
+  }
 
 efficient.portfolio <-
-  function(er, cov.mat, target.return)
+  function(er, cov.mat, target.return, shorts=TRUE)
   {
     # compute minimum variance portfolio subject to target return
     #
@@ -101,6 +105,7 @@ efficient.portfolio <-
     # er					    N x 1 vector of expected returns
     # cov.mat  			  N x N covariance matrix of returns
     # target.return	  scalar, target expected return
+    # shorts          logical, allow shorts is TRUE
     #
     # output is portfolio object with the following elements
     # call				    original function call
@@ -115,8 +120,9 @@ efficient.portfolio <-
     #
     asset.names <- names(er)
     er <- as.vector(er)					# assign names if none exist
+    N <- length(er)
     cov.mat <- as.matrix(cov.mat)
-    if(length(er) != nrow(cov.mat))
+    if(N != nrow(cov.mat))
       stop("invalid inputs")
     if(any(diag(chol(cov.mat)) <= 0))
       stop("Covariance matrix not positive definite")
@@ -125,18 +131,29 @@ efficient.portfolio <-
     #
     # compute efficient portfolio
     #
-    ones <- rep(1, length(er))
-    top <- cbind(2*cov.mat, er, ones)
-    bot <- cbind(rbind(er, ones), matrix(0,2,2))
-    A <- rbind(top, bot)
-    b.target <- as.matrix(c(rep(0, length(er)), target.return, 1))
-    x <- solve(A, b.target)
-    w <- x[1:length(er)]
-    names(w) <- asset.names
+    if(shorts==TRUE){
+      ones <- rep(1, N)
+      top <- cbind(2*cov.mat, er, ones)
+      bot <- cbind(rbind(er, ones), matrix(0,2,2))
+      A <- rbind(top, bot)
+      b.target <- as.matrix(c(rep(0, N), target.return, 1))
+      x <- solve(A, b.target)
+      w <- x[1:N]
+    } else if(shorts==FALSE){
+      Dmat <- 2*cov.mat
+      dvec <- rep.int(0, N)
+      Amat <- cbind(rep(1,N), er, diag(1,N))
+      bvec <- c(1, target.return, rep(0,N))
+      result <- solve.QP(Dmat=Dmat,dvec=dvec,Amat=Amat,bvec=bvec,meq=2)
+      w <- round(result$solution, 6)
+    } else {
+      stop("shorts needs to be logical. For no-shorts, shorts=FALSE.")
+    }
     
     #
     # compute portfolio expected returns and variance
     #
+    names(w) <- asset.names
     er.port <- crossprod(er,w)
     sd.port <- sqrt(w %*% cov.mat %*% w)
     ans <- list("call" = call,
@@ -147,68 +164,69 @@ efficient.portfolio <-
     ans
   }
 
-globalMin.portfolio <- function(er, cov.mat, allow.short=TRUE)
-{
-  # Compute global minimum variance portfolio
-  #
-  # inputs:
-  # er				N x 1 vector of expected returns
-  # cov.mat		N x N return covariance matrix
-  #
-  # output is portfolio object with the following elements
-  # call			original function call
-  # er				portfolio expected return
-  # sd				portfolio standard deviation
-  # weights		N x 1 vector of portfolio weights
-  call <- match.call()
-  
-  #
-  # check for valid inputs
-  #
-  asset.names <- names(er)
-  er <- as.vector(er)					# assign names if none exist
-  cov.mat <- as.matrix(cov.mat)
-  if(length(er) != nrow(cov.mat))
-    stop("invalid inputs")
-  if(any(diag(chol(cov.mat)) <= 0))
-    stop("Covariance matrix not positive definite")
-  # remark: could use generalized inverse if cov.mat is positive semi-definite
-  
-  #
-  # compute global minimum portfolio
-  #
-  if(allow.short) {
-    cov.mat.inv <- solve(cov.mat)
-    one.vec <- rep(1,length(er))
-    #  w.gmin <- cov.mat.inv %*% one.vec/as.vector(one.vec %*% cov.mat.inv %*% one.vec)
-    w.gmin <- rowSums(cov.mat.inv) / sum(cov.mat.inv)
-    w.gmin <- as.vector(w.gmin)
+globalMin.portfolio <-
+  function(er, cov.mat, shorts=TRUE)
+  {
+    # Compute global minimum variance portfolio
+    #
+    # inputs:
+    # er				N x 1 vector of expected returns
+    # cov.mat		N x N return covariance matrix
+    # shorts          logical, allow shorts is TRUE
+    #
+    # output is portfolio object with the following elements
+    # call			original function call
+    # er				portfolio expected return
+    # sd				portfolio standard deviation
+    # weights		N x 1 vector of portfolio weights
+    call <- match.call()
+    
+    #
+    # check for valid inputs
+    #
+    asset.names <- names(er)
+    er <- as.vector(er)					# assign names if none exist
+    cov.mat <- as.matrix(cov.mat)
+    N <- length(er)
+    if(N != nrow(cov.mat))
+      stop("invalid inputs")
+    if(any(diag(chol(cov.mat)) <= 0))
+      stop("Covariance matrix not positive definite")
+    # remark: could use generalized inverse if cov.mat is positive semi-definite
+    
+    #
+    # compute global minimum portfolio
+    #
+    if(shorts==TRUE){
+      cov.mat.inv <- solve(cov.mat)
+      one.vec <- rep(1,N)
+      w.gmin <- rowSums(cov.mat.inv) / sum(cov.mat.inv)
+      w.gmin <- as.vector(w.gmin)
+    } else if(shorts==FALSE){
+      Dmat <- 2*cov.mat
+      dvec <- rep.int(0, N)
+      Amat <- cbind(rep(1,N), diag(1,N))
+      bvec <- c(1, rep(0,N))
+      result <- solve.QP(Dmat=Dmat,dvec=dvec,Amat=Amat,bvec=bvec,meq=1)
+      w.gmin <- round(result$solution, 6)
+    } else {
+      stop("shorts needs to be logical. For no-shorts, shorts=FALSE.")
+    }
+    
     names(w.gmin) <- asset.names
     er.gmin <- crossprod(w.gmin,er)
     sd.gmin <- sqrt(t(w.gmin) %*% cov.mat %*% w.gmin)
-  } else {
-    len <- length(er)
-    D.mat <- 2*cov.mat
-    d.vec <- rep(0, len)
-    A.mat <- cbind(rep(1,len), diag(len))
-    b.vec <- c(1, rep(0,len))
-    qp.out <- solve.QP(Dmat=D.mat, dvec=d.vec, Amat=A.mat, bvec=b.vec, meq=1)
-    w.gmin <- as.vector(qp.out$solution)
-    names(w.gmin) <- asset.names
-    er.gmin <- as.numeric(crossprod(w.gmin, er))
-    sd.gmin <- sqrt(as.numeric(qp.out$value))
+    gmin.port <- list("call" = call,
+                      "er" = as.vector(er.gmin),
+                      "sd" = as.vector(sd.gmin),
+                      "weights" = w.gmin)
+    class(gmin.port) <- "portfolio"
+    gmin.port
   }
-  gmin.port <- list("call" = call,
-                    "er" = as.vector(er.gmin),
-                    "sd" = as.vector(sd.gmin),
-                    "weights" = w.gmin)
-  class(gmin.port) <- "portfolio"
-  gmin.port
-}
 
 
 tangency.portfolio <- 
-function(er,cov.mat,risk.free)
+  function(er,cov.mat,risk.free, shorts=TRUE)
   {
     # compute tangency portfolio
     #
@@ -216,6 +234,7 @@ function(er,cov.mat,risk.free)
     # er				   N x 1 vector of expected returns
     # cov.mat		   N x N return covariance matrix
     # risk.free		 scalar, risk-free rate
+    # shorts          logical, allow shorts is TRUE
     #
     # output is portfolio object with the following elements
     # call			  captures function call
@@ -232,7 +251,8 @@ function(er,cov.mat,risk.free)
       stop("Risk-free rate must be positive")
     er <- as.vector(er)
     cov.mat <- as.matrix(cov.mat)
-    if(length(er) != nrow(cov.mat))
+    N <- length(er)
+    if(N != nrow(cov.mat))
       stop("invalid inputs")
     if(any(diag(chol(cov.mat)) <= 0))
       stop("Covariance matrix not positive definite")
@@ -241,16 +261,29 @@ function(er,cov.mat,risk.free)
     #
     # compute global minimum variance portfolio
     #
-    gmin.port <- globalMin.portfolio(er,cov.mat)
+    gmin.port <- globalMin.portfolio(er, cov.mat, shorts=shorts)
     if(gmin.port$er < risk.free)
       stop("Risk-free rate greater than avg return on global minimum variance portfolio")
     
     # 
     # compute tangency portfolio
     #
-    cov.mat.inv <- solve(cov.mat)
-    w.t <- cov.mat.inv %*% (er - risk.free) # tangency portfolio
-    w.t <- as.vector(w.t/sum(w.t))	# normalize weights
+    if(shorts==TRUE){
+      cov.mat.inv <- solve(cov.mat)
+      w.t <- cov.mat.inv %*% (er - risk.free) # tangency portfolio
+      w.t <- as.vector(w.t/sum(w.t))	# normalize weights
+    } else if(shorts==FALSE){
+      Dmat <- 2*cov.mat
+      dvec <- rep.int(0, N)
+      er.excess <- er - risk.free
+      Amat <- cbind(er.excess, diag(1,N))
+      bvec <- c(1, rep(0,N))
+      result <- solve.QP(Dmat=Dmat,dvec=dvec,Amat=Amat,bvec=bvec,meq=1)
+      w.t <- round(result$solution/sum(result$solution), 6)
+    } else {
+      stop("Shorts needs to be logical. For no-shorts, shorts=FALSE.")
+    }
+    
     names(w.t) <- asset.names
     er.t <- crossprod(w.t,er)
     sd.t <- sqrt(t(w.t) %*% cov.mat %*% w.t)
@@ -263,7 +296,7 @@ function(er,cov.mat,risk.free)
   }
 
 efficient.frontier <- 
-  function(er, cov.mat, nport=20, alpha.min=-0.5, alpha.max=1.5)
+  function(er, cov.mat, nport=20, alpha.min=-0.5, alpha.max=1.5, shorts=TRUE)
   {
     # Compute efficient frontier with no short-sales constraints
     #
@@ -271,6 +304,7 @@ efficient.frontier <-
     # er			  N x 1 vector of expected returns
     # cov.mat	  N x N return covariance matrix
     # nport		  scalar, number of efficient portfolios to compute
+    # shorts          logical, allow shorts is TRUE
     #
     # output is a Markowitz object with the following elements
     # call		  captures function call
@@ -284,8 +318,9 @@ efficient.frontier <-
     #
     asset.names <- names(er)
     er <- as.vector(er)
+    N <- length(er)
     cov.mat <- as.matrix(cov.mat)
-    if(length(er) != nrow(cov.mat))
+    if(N != nrow(cov.mat))
       stop("invalid inputs")
     if(any(diag(chol(cov.mat)) <= 0))
       stop("Covariance matrix not positive definite")
@@ -301,22 +336,32 @@ efficient.frontier <-
     # compute global minimum variance portfolio
     #
     cov.mat.inv <- solve(cov.mat)
-    one.vec <- rep(1,length(er))
-    port.gmin <- globalMin.portfolio(er,cov.mat)
+    one.vec <- rep(1, N)
+    port.gmin <- globalMin.portfolio(er, cov.mat, shorts)
     w.gmin <- port.gmin$weights
     
-    #
-    # compute efficient frontier as convex combinations of two efficient portfolios
-    # 1st efficient port: global min var portfolio
-    # 2nd efficient port: min var port with ER = max of ER for all assets
-    #
-    er.max <- max(er)
-    port.max <- efficient.portfolio(er,cov.mat,er.max)
-    w.max <- port.max$weights    
-    a <- seq(from=alpha.min,to=alpha.max,length=nport)			# convex combinations
-    we.mat <- a %o% w.gmin + (1-a) %o% w.max	# rows are efficient portfolios
-    er.e <- we.mat %*% er							# expected returns of efficient portfolios
-    er.e <- as.vector(er.e)
+    if(shorts==TRUE){
+      # compute efficient frontier as convex combinations of two efficient portfolios
+      # 1st efficient port: global min var portfolio
+      # 2nd efficient port: min var port with ER = max of ER for all assets
+      er.max <- max(er)
+      port.max <- efficient.portfolio(er,cov.mat,er.max)
+      w.max <- port.max$weights    
+      a <- seq(from=alpha.min,to=alpha.max,length=nport)			# convex combinations
+      we.mat <- a %o% w.gmin + (1-a) %o% w.max	# rows are efficient portfolios
+      er.e <- we.mat %*% er							# expected returns of efficient portfolios
+      er.e <- as.vector(er.e)
+    } else if(shorts==FALSE){
+      we.mat <- matrix(0, nrow=nport, ncol=N)
+      we.mat[1,] <- w.gmin
+      we.mat[nport, which.max(er)] <- 1
+      er.e <- as.vector(seq(from=port.gmin$er, to=max(er), length=nport))
+      for(i in 2:(nport-1)) 
+        we.mat[i,] <- efficient.portfolio(er, cov.mat, er.e[i], shorts)$weights
+    } else {
+      stop("shorts needs to be logical. For no-shorts, shorts=FALSE.")
+    }
+    
     names(er.e) <- port.names
     cov.e <- we.mat %*% cov.mat %*% t(we.mat) # cov mat of efficient portfolios
     sd.e <- sqrt(diag(cov.e))					# std devs of efficient portfolios
